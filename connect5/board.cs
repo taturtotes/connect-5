67420837b127397dc5dd24a7fd6abcd787744025
taturtotes <103700568+taturtotes@users.noreply.github.com>
2023-04-20 11:55:04 -0500

    added IsValid function

--- connect5/board.cs
+++ connect5/board.cs
@@ -211,5 +211,28 @@ namespace connect5
             board1.Controls.Clear();
             turnPanel.BackColor = SystemColors.Control;
         }
+        bool IsValid(int colInstert)
+        {
+            int countCol = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] != 0 && j == colInstert)
+                    {
+                        countCol++;
+                    }
+                }
+            }
+
+            if(countCol == 7)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
