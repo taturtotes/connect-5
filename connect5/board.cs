e5c25fff67ea302eac22b1db8e281f6405edb258
lduff0604 <lduff0604@gmail.com>
2023-04-20 22:17:32 -0500

    Computer Buttons now work

--- connect5/board.cs
+++ connect5/board.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -20,24 +21,39 @@ namespace connect5
 
     public partial class board : Form
     {
+        //Tells type of player
         bool human1;
         bool human2;
+        bool computer1;
+        bool computer2;
+
         //Row piece was inserted in
         int rowInsert;
         //Column piece was inserted in
         int colInsert;
+
+        //Counts number of turns
         int count;
+
+        //Tells whose turn it is
         int turn;
+
         int rowCount = 7;
         int colCount = 6;
+
+        //Holds board
         int[,] matrix = new int[7, 6];
+
+        //Contains name of exe file
+        string comp1File;
+        string comp2File;
 
         //Properties of each piece
         struct panelProp
         {
-           public int row;
-           public int col;
-           public int color;
+            public int row;
+            public int col;
+            public int color;
         }
 
         public board()
@@ -88,11 +104,11 @@ namespace connect5
                 piece.BackColor = Color.Yellow;
             }
 
-            piece.Visible= false;
+            piece.Visible = false;
 
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                if(found == true)
+                if (found == true)
                 {
                     break;
                 }
@@ -115,9 +131,9 @@ namespace connect5
                             row = i;
                             found = true;
                         }
-                     
+
                     }
-                   
+
 
                 }
 
@@ -173,19 +189,20 @@ namespace connect5
                     if (matrix[i, j] == 1)
                     {
                         //Red
-                        outBoard += "1";
+                        outBoard += "1 ";
                     }
-                    else if (matrix[i,j] == 2)
+                    else if (matrix[i, j] == 2)
                     {
                         //Yellow
-                        outBoard += "2";
+                        outBoard += "2 ";
                     }
                     else
                     {
                         //Blank
-                        outBoard += "0";
+                        outBoard += "0 ";
                     }
                 }
+                outBoard += "\n";
             }
 
             using (StreamWriter writetext = new StreamWriter("board.txt"))
@@ -198,12 +215,12 @@ namespace connect5
 
         private void HumanButtonRed_Click(object sender, EventArgs e)
         {
-            human1= true;
+            human1 = true;
         }
 
         private void HumanButton2_Click(object sender, EventArgs e)
         {
-            human2= true;
+            human2 = true;
         }
 
         private void startButton_Click(object sender, EventArgs e)
@@ -211,17 +228,29 @@ namespace connect5
 
             turnPanel.BackColor = Color.Red;
 
+            //Starts exe files
+            if (computer1 == true)
+            {
+                Process.Start(comp1File);
+            }
+
+            if (computer2 == true)
+            {
+                Process.Start(comp1File);
+            }
+
+
         }
 
         private void board1_Click(object sender, EventArgs e)
         {
-            var cellPos = GetRowColIndex(board1,board1.PointToClient(Cursor.Position));
+            var cellPos = GetRowColIndex(board1, board1.PointToClient(Cursor.Position));
             playerInfo Player = new playerInfo();
-            
+
             //Counting number of turns
             count++;
 
-            if(count % 2 == 1)
+            if (count % 2 == 1)
             {
                 //Red
                 turn = 1;
@@ -236,13 +265,13 @@ namespace connect5
 
             outputBoard();
 
-             Panel piece = makePanel(turn, colInsert, rowInsert);
-             piece.Visible = true;
+            Panel piece = makePanel(turn, colInsert, rowInsert);
+            piece.Visible = true;
 
 
             //Outboard
             //If valid make piece visible
-           
+
             //Delete if not valid
 
             if (count % 2 == 1)
@@ -259,7 +288,9 @@ namespace connect5
 
             outputBoard();
 
-            //if (IsWinner(colInsert, rowInsert,Player))
+            IsWinner(colInsert, rowInsert, Player);
+
+            //if (IsWinner(colInsert, rowInsert, Player))
             //{
             //    //Show game over screen & call reset
             //}
@@ -271,7 +302,7 @@ namespace connect5
         {
             foreach (Control ctrl in board1.Controls)
             {
-                ctrl.Visible= false;
+                ctrl.Visible = false;
             }
 
             board1.Controls.Clear();
@@ -284,7 +315,7 @@ namespace connect5
                 }
             }
 
-         }
+        }
         bool IsValid(int colInstert)
         {
             int countCol = 0;
@@ -299,7 +330,7 @@ namespace connect5
                 }
             }
 
-            if(countCol == 7)
+            if (countCol == 7)
             {
                 return false;
             }
@@ -309,42 +340,68 @@ namespace connect5
             }
         }
 
-        bool IsWinner(int colInsert, int rowInsert,  playerInfo Player)
+        bool IsWinner(int colInsert, int rowInsert, playerInfo Player)
         {
             int currPlayer = Player.playerID;
 
-            for(int i = 6; i >= 1; --i )
+            for (int i = 6; i >= 1; --i)
             {
-                for(int j = 5; j >= 1; --j)
+                for (int j = 5; j >= 1; --j)
                 {
-                    if (matrix[i, j] == currPlayer && matrix[i-1,j-1] == currPlayer && matrix[i - 2, j - 2] == currPlayer &&
-                        matrix[i - 3, j - 3] == currPlayer &&  matrix[i - 4, j - 4] == currPlayer)
+                    if (matrix[i, j] == currPlayer && matrix[i - 1, j - 1] == currPlayer && matrix[i - 2, j - 2] == currPlayer &&
+                        matrix[i - 3, j - 3] == currPlayer && matrix[i - 4, j - 4] == currPlayer)
                     {
                         return true;
                     }
-                    if (matrix[i,j] == currPlayer && matrix[i, j-1] == currPlayer && matrix[i, j - 2] == currPlayer &&
+                    if (matrix[i, j] == currPlayer && matrix[i, j - 1] == currPlayer && matrix[i, j - 2] == currPlayer &&
                         matrix[i, j - 3] == currPlayer && matrix[i, j - 4] == currPlayer)
                     {
                         return true;
                     }
-                    if(matrix[i, j] == currPlayer && matrix[i-1, j] == currPlayer && matrix[i-2, j] == currPlayer &&
-                       matrix[i - 3, j] == currPlayer && matrix[i -4, j] == currPlayer)
+                    if (matrix[i, j] == currPlayer && matrix[i - 1, j] == currPlayer && matrix[i - 2, j] == currPlayer &&
+                       matrix[i - 3, j] == currPlayer && matrix[i - 4, j] == currPlayer)
                     {
                         return true;
                     }
-                    if(matrix[i, j] == currPlayer && matrix[i - 1, j+1] == currPlayer && matrix[i - 2, j +2] == currPlayer &&
-                       matrix[i - 3, j +3] == currPlayer && matrix[i - 4, j +4] == currPlayer)
+                    if (matrix[i, j] == currPlayer && matrix[i - 1, j + 1] == currPlayer && matrix[i - 2, j + 2] == currPlayer &&
+                       matrix[i - 3, j + 3] == currPlayer && matrix[i - 4, j + 4] == currPlayer)
                     {
                         return true;
                     }
-                    if(matrix[i, j] == currPlayer && matrix[i , j + 1] != 0 && matrix[i , j  + 2] == currPlayer &&
-                       matrix[i , j + 3] == currPlayer && matrix[i , j + 4] == currPlayer)
+                    if (matrix[i, j] == currPlayer && matrix[i, j + 1] != 0 && matrix[i, j + 2] == currPlayer &&
+                       matrix[i, j + 3] == currPlayer && matrix[i, j + 4] == currPlayer)
                     {
                         return true;
                     }
                 }
             }
             return false;
+        }
+
+
+
+        private void ComputerButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                comp1File = openFileDialog1.FileName;
+                computer1 = true;
+
+            }
+        }
+
+        private void ComputerButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                comp2File = openFileDialog1.FileName;
+                computer2 = true;
+
+            }
         }
     }
 }
